d7d5ab6e94964bed650261930abe4f4b9a3966d4
jonas1015 <jonasgsmart1015@gmail.com>
2023-03-16 18:27:58 +0300

    More functionality in desktop app to include settings

--- Homepage.Designer.cs
+++ Homepage.Designer.cs
@@ -1,7 +1,6 @@
 ﻿using Fleck;
 using System;
 using System.Windows.Forms;
-using System.Text.Json;
 using System.Text;
 
 namespace WebPrinting
@@ -34,21 +33,36 @@ namespace WebPrinting
         /// </summary>
         private void InitializeComponent()
         {
-            this.loadPrinters = new System.Windows.Forms.TabPage();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Homepage));
             this.printButton = new System.Windows.Forms.Button();
             this.printersList = new System.Windows.Forms.ListBox();
             this.reloadPrinters = new System.Windows.Forms.Label();
             this.setupTab = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.welcomePage = new System.Windows.Forms.TabPage();
+            this.printersListPage = new System.Windows.Forms.TabPage();
+            this.printersSettings = new System.Windows.Forms.DataGridView();
+            this.settingsPage = new System.Windows.Forms.TabPage();
+            this.zplCommands = new System.Windows.Forms.TextBox();
+            this.printers = new System.Windows.Forms.ComboBox();
+            this.savePrintersSettingsButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.useThisPrinter = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.savePrintServerSettingsButton = new System.Windows.Forms.Button();
+            this.printServerAutoStart = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.welcomePage.SuspendLayout();
+            this.printersListPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.printersSettings)).BeginInit();
+            this.settingsPage.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // loadPrinters
-            // 
-            this.loadPrinters.Location = new System.Drawing.Point(0, 0);
-            this.loadPrinters.Name = "loadPrinters";
-            this.loadPrinters.Size = new System.Drawing.Size(200, 100);
-            this.loadPrinters.TabIndex = 0;
             // 
             // printButton
             // 
@@ -82,7 +96,7 @@ namespace WebPrinting
             // 
             this.label1.Font = new System.Drawing.Font("Malgun Gothic", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.label1.Location = new System.Drawing.Point(139, 95);
+            this.label1.Location = new System.Drawing.Point(131, 244);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(522, 56);
             this.label1.TabIndex = 2;
@@ -92,83 +106,196 @@ namespace WebPrinting
             // 
             this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label2.Location = new System.Drawing.Point(209, 298);
+            this.label2.Location = new System.Drawing.Point(211, 338);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(360, 38);
             this.label2.TabIndex = 3;
             this.label2.Text = "Minimize this window and continue to work";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Print Server";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon1_MouseDoubleClick);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.welcomePage);
+            this.tabControl1.Controls.Add(this.printersListPage);
+            this.tabControl1.Controls.Add(this.settingsPage);
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(783, 434);
+            this.tabControl1.TabIndex = 4;
+            // 
+            // welcomePage
+            // 
+            this.welcomePage.Controls.Add(this.label1);
+            this.welcomePage.Controls.Add(this.label2);
+            this.welcomePage.Location = new System.Drawing.Point(4, 22);
+            this.welcomePage.Name = "welcomePage";
+            this.welcomePage.Padding = new System.Windows.Forms.Padding(3);
+            this.welcomePage.Size = new System.Drawing.Size(775, 408);
+            this.welcomePage.TabIndex = 0;
+            this.welcomePage.Text = "Welcome";
+            this.welcomePage.UseVisualStyleBackColor = true;
+            // 
+            // printersListPage
+            // 
+            this.printersListPage.Controls.Add(this.printersSettings);
+            this.printersListPage.Location = new System.Drawing.Point(4, 22);
+            this.printersListPage.Name = "printersListPage";
+            this.printersListPage.Size = new System.Drawing.Size(775, 408);
+            this.printersListPage.TabIndex = 2;
+            this.printersListPage.Text = "Printers List";
+            this.printersListPage.UseVisualStyleBackColor = true;
+            // 
+            // printersSettings
+            // 
+            this.printersSettings.AllowUserToAddRows = false;
+            this.printersSettings.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.printersSettings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.printersSettings.Location = new System.Drawing.Point(0, 0);
+            this.printersSettings.Name = "printersSettings";
+            this.printersSettings.Size = new System.Drawing.Size(775, 408);
+            this.printersSettings.TabIndex = 0;
+            // 
+            // settingsPage
+            // 
+            this.settingsPage.Controls.Add(this.zplCommands);
+            this.settingsPage.Controls.Add(this.printers);
+            this.settingsPage.Controls.Add(this.savePrintersSettingsButton);
+            this.settingsPage.Controls.Add(this.label5);
+            this.settingsPage.Controls.Add(this.label6);
+            this.settingsPage.Controls.Add(this.useThisPrinter);
+            this.settingsPage.Controls.Add(this.label4);
+            this.settingsPage.Controls.Add(this.savePrintServerSettingsButton);
+            this.settingsPage.Controls.Add(this.printServerAutoStart);
+            this.settingsPage.Controls.Add(this.label3);
+            this.settingsPage.Location = new System.Drawing.Point(4, 22);
+            this.settingsPage.Name = "settingsPage";
+            this.settingsPage.Padding = new System.Windows.Forms.Padding(3);
+            this.settingsPage.Size = new System.Drawing.Size(775, 408);
+            this.settingsPage.TabIndex = 1;
+            this.settingsPage.Text = "Settings";
+            this.settingsPage.UseVisualStyleBackColor = true;
+            // 
+            // zplCommands
+            // 
+            this.zplCommands.Location = new System.Drawing.Point(114, 173);
+            this.zplCommands.Multiline = true;
+            this.zplCommands.Name = "zplCommands";
+            this.zplCommands.Size = new System.Drawing.Size(375, 189);
+            this.zplCommands.TabIndex = 13;
+            // 
+            // printers
+            // 
+            this.printers.FormattingEnabled = true;
+            this.printers.Location = new System.Drawing.Point(114, 132);
+            this.printers.Name = "printers";
+            this.printers.Size = new System.Drawing.Size(375, 21);
+            this.printers.TabIndex = 12;
+            // 
+            // savePrintersSettingsButton
+            // 
+            this.savePrintersSettingsButton.Location = new System.Drawing.Point(614, 321);
+            this.savePrintersSettingsButton.Name = "savePrintersSettingsButton";
+            this.savePrintersSettingsButton.Size = new System.Drawing.Size(107, 41);
+            this.savePrintersSettingsButton.TabIndex = 11;
+            this.savePrintersSettingsButton.Text = "Save ";
+            this.savePrintersSettingsButton.UseVisualStyleBackColor = true;
+            this.savePrintersSettingsButton.Click += new System.EventHandler(this.SavePrintersSettingsButton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(18, 173);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ZPL Commands";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(18, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Printer Name";
+            // 
+            // useThisPrinter
+            // 
+            this.useThisPrinter.AutoSize = true;
+            this.useThisPrinter.Location = new System.Drawing.Point(586, 132);
+            this.useThisPrinter.Name = "useThisPrinter";
+            this.useThisPrinter.Size = new System.Drawing.Size(135, 17);
+            this.useThisPrinter.TabIndex = 5;
+            this.useThisPrinter.Text = "Use this printer settings";
+            this.useThisPrinter.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Printers Settings";
+            // 
+            // savePrintServerSettingsButton
+            // 
+            this.savePrintServerSettingsButton.Location = new System.Drawing.Point(325, 40);
+            this.savePrintServerSettingsButton.Name = "savePrintServerSettingsButton";
+            this.savePrintServerSettingsButton.Size = new System.Drawing.Size(75, 23);
+            this.savePrintServerSettingsButton.TabIndex = 2;
+            this.savePrintServerSettingsButton.Text = "Save ";
+            this.savePrintServerSettingsButton.UseVisualStyleBackColor = true;
+            this.savePrintServerSettingsButton.Click += new System.EventHandler(this.SavePrintServerSettingsButton_Click);
+            // 
+            // printServerAutoStart
+            // 
+            this.printServerAutoStart.AutoSize = true;
+            this.printServerAutoStart.Location = new System.Drawing.Point(21, 44);
+            this.printServerAutoStart.Name = "printServerAutoStart";
+            this.printServerAutoStart.Size = new System.Drawing.Size(131, 17);
+            this.printServerAutoStart.TabIndex = 1;
+            this.printServerAutoStart.Text = "Print Server Auto Start";
+            this.printServerAutoStart.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label3.Location = new System.Drawing.Point(18, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Print Server Settings";
             // 
             // Homepage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Homepage";
-            this.Text = "Homepage";
+            this.Text = "Print Server";
+            this.Load += new System.EventHandler(this.Homepage_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.welcomePage.ResumeLayout(false);
+            this.printersListPage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.printersSettings)).EndInit();
+            this.settingsPage.ResumeLayout(false);
+            this.settingsPage.PerformLayout();
             this.ResumeLayout(false);
-
-            //
-            //Initiate Socket server as soon as the application is run
-            //
-            
-            var server = new WebSocketServer("ws://127.0.0.1:4444");
-            server.RestartAfterListenError = true;
-            server.Start(socket =>
-            {
-                socket.OnOpen = () => 
-                {
-                    string welcomeMessage = JsonSerializer.Serialize(new Message
-                    {
-                        type = "welcome",
-                        message = "Connected"
-                    });
-                    socket.Send(welcomeMessage);
-                };
-                socket.OnClose = () => Console.WriteLine("Client disconnected");
-                socket.OnMessage = (message) =>
-                {
-                    Message messageObject = JsonSerializer.Deserialize<Message>(message);
-                    if (messageObject.type.ToLower() == "print")
-                    {
-                        try
-                        {
-                            messageObject.PrintMessage();
-                            string successMessage = JsonSerializer.Serialize(new Message
-                            {
-                                type = "Success",
-                                message = "Print job is completed"
-                            });
-                            socket.Send(successMessage);
-                        }
-                        catch (Exception ex)
-                        {
-                            string exceptionMessage = JsonSerializer.Serialize(new Message
-                            {
-                                type = "Error",
-                                message = $"{ex.Message}"
-                            });
-                            socket.Send(exceptionMessage);
-                        }
-                    } 
-                    else
-                    {
-                        messageObject.PrintMessage();
-                        string unsupportedMessage = JsonSerializer.Serialize(new Message
-                        {
-                            type = "unsupported",
-                            message = $"{messageObject.type} type of message is unsupported."
-                        });
-                        socket.Send(unsupportedMessage);
-                    }
-                };
-            });
 
         }
 
         #endregion
-        private TabPage loadPrinters;
         private TabPage setupTab;
         private ListBox printersList;
         private Label reloadPrinters;
@@ -177,6 +304,23 @@ namespace WebPrinting
         private string selectedPrinter;
         private Label label1;
         private Label label2;
+        private NotifyIcon notifyIcon1;
+        private TabControl tabControl1;
+        private TabPage welcomePage;
+        private TabPage settingsPage;
+        private CheckBox useThisPrinter;
+        private Label label4;
+        private Button savePrintServerSettingsButton;
+        private CheckBox printServerAutoStart;
+        private Label label3;
+        private Label label6;
+        private ComboBox printers;
+        private Button savePrintersSettingsButton;
+        private Label label5;
+        private TabPage printersListPage;
+        private string dbConnectionString = "Data Source=WebPrintServerDB.db;Version=3;";
+        private TextBox zplCommands;
+        private DataGridView printersSettings;
     }
 }
 
--- Homepage.cs
+++ Homepage.cs
@@ -5,6 +5,11 @@ using System.Drawing.Printing;
 using Zebra.Sdk.Printer;
 using Zebra.Sdk.Comm;
 using Zebra.Sdk.Printer.Discovery;
+using Fleck;
+using System.Text.Json;
+using System.Data.SQLite;
+using System.Data;
+using System.Drawing.Printing;
 
 namespace WebPrinting
 {
@@ -14,13 +19,212 @@ namespace WebPrinting
         {
             InitializeComponent();
         }
+
+        private void Homepage_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string query = $"SELECT * FROM GeneralSettings;";
+
+                SQLiteConnection connection = new SQLiteConnection(this.dbConnectionString);
+                connection.Open();
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                dataAdapter.Fill(dataTable);
+                LoadPrinters();
+                LoadAvailablePrinters();
+                if (dataTable.Rows.Count > 0 && dataTable.Rows[0]["autoStart"].ToString() == "1")
+                {
+                    this.printServerAutoStart.Checked = true;
+                    //Hide window
+                    this.WindowState = FormWindowState.Minimized;
+                    //
+                    // Initiate Socket server as soon as the application is run
+                    //
+                    this.StartServer();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error occured when starting an application. Error: {ex.Message}");
+            }
+        }
+
+        private void StartServer()
+        {
+            WebSocketServer server = new WebSocketServer("ws://127.0.0.1:4444")
+            {
+                RestartAfterListenError = true
+            };
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    string welcomeMessage = JsonSerializer.Serialize(new SocketMessage
+                    {
+                        Type = "welcome",
+                        Message = "Connected"
+                    });
+                    socket.Send(welcomeMessage);
+                    Console.WriteLine("Client connected");
+                };
+                socket.OnClose = () => Console.WriteLine("Client disconnected");
+                socket.OnMessage = (message) =>
+                {
+                    SocketMessage messageObject = JsonSerializer.Deserialize<SocketMessage>(message);
+                    if (messageObject.Type.ToLower() == "print")
+                    {
+                        try
+                        {
+                            messageObject.PrintMessage();
+                            string successMessage = JsonSerializer.Serialize(new SocketMessage
+                            {
+                                Type = "Success",
+                                Message = "Print job is completed"
+                            });
+                            socket.Send(successMessage);
+                        }
+                        catch (Exception ex)
+                        {
+                            string exceptionMessage = JsonSerializer.Serialize(new SocketMessage
+                            {
+                                Type = "Error",
+                                Message = $"{ex.Message}"
+                            });
+                            socket.Send(exceptionMessage);
+                        }
+                    }
+                    else
+                    {
+                        messageObject.PrintMessage();
+                        string unsupportedMessage = JsonSerializer.Serialize(new SocketMessage
+                        {
+                            Type = "unsupported",
+                            Message = $"{messageObject.Type} type of message is unsupported."
+                        });
+                        socket.Send(unsupportedMessage);
+                    }
+                };
+            });
+        }
+
+        private void LoadPrinters()
+        {
+
+            // Get all installed printers
+            PrinterSettings.StringCollection printers = PrinterSettings.InstalledPrinters;
+
+            // Loop through each printer and output its name
+            foreach (string printer in printers)
+            {
+               this.printers.Items.Add(printer);
+            }
+
+        }
+
+        private void LoadAvailablePrinters()
+        {
+            string query = $"SELECT * FROM PrinterSettings;";
+
+            SQLiteConnection connection = new SQLiteConnection(this.dbConnectionString);
+            connection.Open();
+            SQLiteCommand command = new SQLiteCommand(query, connection);
+            SQLiteDataAdapter dataAdapter = new SQLiteDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            this.printersSettings.DataSource = dataTable;
+
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                this.Visible = false;
+            }
+            else
+            {
+                base.OnFormClosing(e);
+            }
+        }
+
+        private void NotifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Visible = true;
+            this.WindowState = FormWindowState.Normal;
+
+        }
+
+        private void SavePrintServerSettingsButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var autostartValue = this.printServerAutoStart.Checked;
+                int autoStart = 0;
+                if (autostartValue)
+                {
+                    autoStart = 1;
+                }
+
+                string query = $"UPDATE GeneralSettings set autoStart={autoStart} WHERE id=1;";
+                SQLiteConnection connection = new SQLiteConnection(this.dbConnectionString);
+                connection.Open();
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                var response = command.ExecuteNonQuery();
+                if (response > 0)
+                {
+                    this.printServerAutoStart.Checked = autostartValue;
+                    MessageBox.Show("Saved successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Couldn't save data into the database.");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Encountered an error when trying to save. Error: {ex.Message}");
+            }
+        }
+
+        private void SavePrintersSettingsButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string printer = this.printers.SelectedItem.ToString();
+                int useThisPrinter = 0;
+                string zplCommands = this.zplCommands.Text;
+                if (this.useThisPrinter.Checked) useThisPrinter = 1;
+
+                string query = $"INSERT INTO PrinterSettings Values (NULL ,'{printer}','{zplCommands}',{useThisPrinter});";
+                SQLiteConnection connection = new SQLiteConnection(this.dbConnectionString);
+                connection.Open();
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                var response = command.ExecuteNonQuery();
+                if (response > 0)
+                {
+                    MessageBox.Show("Printer settings saved successfully");
+                }
+                else
+                {
+                    MessageBox.Show("Couldn't save data into the database.");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error occured while trying to save printer settings. Error: {ex.Message}");
+            }
+        }
     }
 
-    class Message
+    public class SocketMessage
     {
-        public string type { get; set; }
-        public string message { get; set; }
-
+        public string Type { get; set; }
+        public string Message { get; set; }
         public void PrintMessage()
         {
             foreach (DiscoveredUsbPrinter usbPrinter in UsbDiscoverer.GetZebraUsbPrinters(new ZebraPrinterFilter()))
@@ -35,7 +239,7 @@ namespace WebPrinting
                         {
                             ZebraPrinter printer = ZebraPrinterFactory.GetInstance(conn);
 
-                            byte[] zplData = Encoding.ASCII.GetBytes(this.message);
+                            byte[] zplData = Encoding.ASCII.GetBytes(this.Message);
 
                             conn.Write(zplData);
                             conn.Close();
@@ -67,4 +271,5 @@ namespace WebPrinting
 
         }
     }
+
 }
